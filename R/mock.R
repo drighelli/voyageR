a3ccb421b246d17c3b4764350d9f9a0eb028d756
Gabriele Sales <gbrsales@gmail.com>
2021-01-30 12:05:48 +0100

    Test for spatially variable genes
    Initial implementation based on spatialDE only.

---
+++ R/mock.R
@@ -0,0 +1,33 @@
+#' Generate SpatialExperiment instance including spatially variable genes.
+#'
+#' @param tot_genes Total number of gene profiles to be generated.
+#' @param de_genes Number of spatially variable genes.
+#' @param grid_size Genes will be spatially arranged on a size x size grid.
+#'
+#' @return A [SpatialExperiment] object.
+#' 
+#' @importFrom SpatialExperiment SpatialExperiment
+#' @importFrom S4Vectors SimpleList
+#' @importFrom stats rnbinom runif
+#' @export
+#' 
+#' @examples 
+#' mockSVGenes(1000, 10, 100)
+#' 
+mockSVGenes <- function(tot_genes, de_genes, grid_size) {
+  coordinates <- data.frame(
+    x = rep(seq.int(grid_size), grid_size),
+    y = rep(seq.int(grid_size), each=grid_size)
+  )
+  
+  mu <- 2^runif(tot_genes, -1, 5)
+  counts <- matrix(rnbinom(tot_genes*grid_size*grid_size, mu=mu, size=10),
+                   nrow=tot_genes)
+  m <- (grid_size/2)+1
+  mask <- coordinates$x < m & coordinates$y < m
+  counts[seq.int(de_genes), mask] <- counts[seq.int(de_genes), mask] + 20
+  rownames(counts) <- paste0("gene", seq.int(tot_genes))
+  
+  SpatialExperiment(assays = SimpleList(counts = counts),
+                    spatialCoords = coordinates)
+}
---
+++ R/sv.R
@@ -0,0 +1,65 @@
+#' Identifies spatially variable genes.
+#'
+#' You can select one among the following statistical tests:
+#' * spatialde, from [Svensson et al, 2018](https://www.nature.com/articles/nmeth.4636).
+#' * spark, from [Sun et al, 2020](https://www.nature.com/articles/s41592-019-0701-7).
+#'
+#' @param x Spatial gene expression object.
+#' @param method Name of the method for testing spatial variabily.
+#' @param ... Additional arguments.
+#'
+#' @return Extended object with DE information.
+#'
+#' @importFrom methods setGeneric
+#' @export
+#' @docType methods
+#' @rdname svGenes-methods
+setGeneric("svGenes", function(x, method, ...) standardGeneric("svGenes"))
+
+#' Identifies spatially variable gene from the given SpatialExperiment.
+#'
+#' @param x SpatialExpression object.
+#' @param method Name of the method for testing spatial variabily.
+#' @param assay Extract gene expression from this assay.
+#'
+#' @return Extended SpatialExpression object.
+#'
+#' @importFrom methods setMethod
+#' @importFrom SummarizedExperiment assay
+#' @importFrom SpatialExperiment spatialCoords
+#' @export
+#'
+#' @rdname svGenes-methods
+#' @aliases svGenes,SpatialExperiment,ANY-method
+#' 
+#' @examples 
+#' svGenes(mockSVGenes(100, 4, 40), method = "spatialde")
+#' 
+setMethod("svGenes",
+          "SpatialExperiment",
+          function(x,
+                   method = c("spatialde", "spark"),
+                   assay = "counts") {
+            method <- match.arg(method)
+            counts <- SummarizedExperiment::assay(x, assay)
+            switch(method,
+                   spatialde = spatialde_svg(x, counts),
+                   spark = spark_svg(x, counts))
+          })
+
+spatialde_svg <- function(x, counts) {
+  pacman::p_load_gh("sales-lab/spatialDE@wrap_functions")
+  
+  coordinates <- as.data.frame(spatialCoords(x))
+  
+  sample_info <- coordinates
+  sample_info$total_counts <- colSums(counts)
+  
+  stabilized <- spatialDE::stabilize(counts)
+  regressed <- spatialDE::regress_out(sample_info, stabilized)
+  spatialDE::run(coordinates, regressed)
+}
+
+spark_svg <- function(x, counts) {
+  
+}
