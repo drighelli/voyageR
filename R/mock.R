b1994848d84ef808a73314c4204bb7399bd80f2f
Gabriele Sales <gbrsales@gmail.com>
2021-02-05 10:08:58 +0100

    Update mockSVGenes documentation
    Add a reference to the trendsceek package we have used internally.

--- R/mock.R
+++ R/mock.R
@@ -1,25 +1,27 @@
-#' Generate SpatialExperiment instance including spatially variable genes.
+#' Generate a mock SpatialExperiment including spatially variable genes.
+#'
+#' Implementation based on [trendsceek::add_markdist_step()].
 #'
 #' @param tot_genes Total number of gene profiles to be generated.
 #' @param de_genes Number of spatially variable genes.
-#' @param lambda Expected number of points over which genes are sampled.
+#' @param lambda Expected number of spots over which genes are sampled.
 #'
 #' @return A [SpatialExperiment] object.
-#' 
+#'
 #' @importFrom SpatialExperiment SpatialExperiment
 #' @importFrom S4Vectors SimpleList DataFrame
 #' @export
-#' 
-#' @examples 
+#'
+#' @examples
 #' mockSVGenes(20, 2, 20)
-#' 
+#'
 mockSVGenes <- function(tot_genes, de_genes, lambda) {
   pp <- trendsceek::sim_pois(lambda)
-  
+
   low_expr <- rep(10, de_genes)
   high_expr <- rep(50, de_genes)
   pp <- trendsceek::add_markdist_step(pp, low_expr, high_expr)
-  
+
   coordinates <- data.frame(x = pp$x, y = pp$y)
 
   counts <- rbind(
@@ -27,13 +29,13 @@ mockSVGenes <- function(tot_genes, de_genes, lambda) {
     matrix(stats::rpois((tot_genes - de_genes) * nrow(coordinates), 10),
            ncol = nrow(coordinates))
   )
-  
+
   genes <- paste0("gene", seq.int(tot_genes))
   rownames(counts) <- genes
   colnames(counts) <- paste0("spot", seq.int(nrow(coordinates)))
-  
+
   rowData <- DataFrame(gene = genes)
-  
+
   SpatialExperiment(assays = SimpleList(counts = counts),
                     spatialCoords = coordinates,
                     rowData = rowData)
